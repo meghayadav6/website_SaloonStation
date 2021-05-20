5901acf9af6b91883741f0bcc09530f402df175a
megha kumari <meghayadav6n@gmail.com>
2021-05-20 16:52:14 +0530

    web site

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+(function($) {
+    'use strict';
+
+    $('.service-caro').owlCarousel({
+        loop:false,
+        margin:5,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+    $('.test-caro').owlCarousel({
+        autoplay:true,
+        dots: true,
+        loop:true,
+        nav:false,
+        items: 1
+    })
+
+    $('.mobile-menu').on('click', function() {
+        $('.primary-menu ul').slideToggle();
+    })
+
+}) (jQuery)
